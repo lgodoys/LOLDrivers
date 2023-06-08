28f929b31f2db0e70fb9a7707a43bd25089facec
Leonardo Godoy <leonardo.godoy.2009@outlook.com>
2023-06-08 17:23:56 -0400

    Modified "Exceptions.py" to create log folder when doesn´t exists

--- utils/Exceptions.py
+++ utils/Exceptions.py
@@ -3,11 +3,14 @@ import sys
 import os
 
 dirpath = os.path.dirname(os.path.abspath(__file__))
-parentPath = os.path.join(dirpath, os.pardir)
 
 def logger(name):
     formatter = logging.Formatter(fmt='%(asctime)s %(levelname)-8s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
-    handler = logging.FileHandler(parentPath+'/logs/'+name+'.log', mode='a')
+    localPath = os.path.join(dirpath,os.pardir+"/logs/")
+    if not os.path.exists(localPath):
+        os.makedirs(localPath, 0o755)
+    
+    handler = logging.FileHandler(localPath+name+'.log', mode='a')
     handler.setFormatter(formatter)
     screen_handler = logging.StreamHandler(stream=sys.stdout)
     screen_handler.setFormatter(formatter)
